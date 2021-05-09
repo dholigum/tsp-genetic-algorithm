d43a78a2a95da349b341c25b81ab9eb4f77916c0
Syahrul Fadholi Gumelar <46102677+dholigum@users.noreply.github.com>
2021-05-09 21:16:38 +0700

    Adding main tspga class

---
+++ tspga.py
@@ -0,0 +1,222 @@
+import numpy as np
+import random
+import operator
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Define city class
+class City:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def distance(self, city):
+        xDis = abs(self.x - city.x)
+        yDis = abs(self.y - city.y)
+        distance = np.sqrt((xDis ** 2) + (yDis ** 2))
+        return distance
+    
+    def __repr__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+# Define fitness class
+class Fitness:
+    def __init__(self, route):
+        self.route = route
+        self.distance = 0
+        self.fitness= 0.0
+    
+    def routeDistance(self):
+        if self.distance ==0:
+            pathDistance = 0
+            for i in range(0, len(self.route)):
+                fromCity = self.route[i]
+                toCity = None
+                if i + 1 < len(self.route):
+                    toCity = self.route[i + 1]
+                else:
+                    toCity = self.route[0]
+                pathDistance += fromCity.distance(toCity)
+            self.distance = pathDistance
+        return self.distance
+    
+    def routeFitness(self):
+        if self.fitness == 0:
+            self.fitness = 1 / float(self.routeDistance())
+        return self.fitness
+
+def createRoute(cityList):
+    route = random.sample(cityList, len(cityList))
+    return route
+
+def initialPopulation(popSize, cityList):
+    population = []
+
+    for i in range(0, popSize):
+        population.append(createRoute(cityList))
+    return population
+
+def rankRoutes(population):
+    fitnessResults = {}
+    for i in range(0,len(population)):
+        fitnessResults[i] = Fitness(population[i]).routeFitness()
+    return sorted(fitnessResults.items(), key = operator.itemgetter(1), reverse = True)
+
+def selection(popRanked, eliteSize):
+    selectionResults = []
+    df = pd.DataFrame(np.array(popRanked), columns=["Index","Fitness"])
+    df['cum_sum'] = df.Fitness.cumsum()
+    df['cum_perc'] = 100*df.cum_sum/df.Fitness.sum()
+    
+    for i in range(0, eliteSize):
+        selectionResults.append(popRanked[i][0])
+    for i in range(0, len(popRanked) - eliteSize):
+        pick = 100*random.random()
+        for i in range(0, len(popRanked)):
+            if pick <= df.iat[i,3]:
+                selectionResults.append(popRanked[i][0])
+                break
+    return selectionResults
+
+def matingPool(population, selectionResults):
+    matingpool = []
+    for i in range(0, len(selectionResults)):
+        index = selectionResults[i]
+        matingpool.append(population[index])
+    return matingpool
+
+def breed(parent1, parent2):
+    child = []
+    childP1 = []
+    childP2 = []
+    
+    geneA = int(random.random() * len(parent1))
+    geneB = int(random.random() * len(parent1))
+    
+    startGene = min(geneA, geneB)
+    endGene = max(geneA, geneB)
+
+    for i in range(startGene, endGene):
+        childP1.append(parent1[i])
+        
+    childP2 = [item for item in parent2 if item not in childP1]
+
+    child = childP1 + childP2
+    return child
+
+def breedPopulation(matingpool, eliteSize):
+    children = []
+    length = len(matingpool) - eliteSize
+    pool = random.sample(matingpool, len(matingpool))
+
+    for i in range(0,eliteSize):
+        children.append(matingpool[i])
+    
+    for i in range(0, length):
+        child = breed(pool[i], pool[len(matingpool)-i-1])
+        children.append(child)
+    return children
+
+def mutate(individual, mutationRate):
+    for swapped in range(len(individual)):
+        if(random.random() < mutationRate):
+            swapWith = int(random.random() * len(individual))
+            
+            city1 = individual[swapped]
+            city2 = individual[swapWith]
+            
+            individual[swapped] = city2
+            individual[swapWith] = city1
+    return individual
+
+def mutatePopulation(population, mutationRate):
+    mutatedPop = []
+    
+    for ind in range(0, len(population)):
+        mutatedInd = mutate(population[ind], mutationRate)
+        mutatedPop.append(mutatedInd)
+    return mutatedPop
+
+def nextGeneration(currentGen, eliteSize, mutationRate):
+    popRanked = rankRoutes(currentGen)
+    selectionResults = selection(popRanked, eliteSize)
+    matingpool = matingPool(currentGen, selectionResults)
+    children = breedPopulation(matingpool, eliteSize)
+    nextGeneration = mutatePopulation(children, mutationRate)
+    return nextGeneration
+
+def geneticAlgorithm(population, popSize, eliteSize, mutationRate, generations):
+    pop = initialPopulation(popSize, population)
+    print("Initial distance: " + str(1 / rankRoutes(pop)[0][1]))
+    
+    for i in range(0, generations):
+        pop = nextGeneration(pop, eliteSize, mutationRate)
+    
+    print("Final distance: " + str(1 / rankRoutes(pop)[0][1]))
+    bestRouteIndex = rankRoutes(pop)[0][0]
+    bestRoute = pop[bestRouteIndex]
+    return bestRoute
+
+# Plot fitness score
+def geneticAlgorithmPlot(population, popSize, eliteSize, mutationRate, generations, dataset):
+    pop = initialPopulation(popSize, population)
+    progress = []
+    progress.append(1 / rankRoutes(pop)[0][1])
+    
+    for i in range(0, generations):
+        pop = nextGeneration(pop, eliteSize, mutationRate)
+        progress.append(1 / rankRoutes(pop)[0][1])
+    
+    plt.plot(progress)
+    plt.title('Fitness score each generation of {0} dataset'.format(dataset))
+    plt.ylabel('Distance')
+    plt.xlabel('Generation')
+    plt.show()
+
+# Plot map visualization
+def plotTSP(paths, points, num_iters=1, dataset='else'):
+    x = []; y = []
+    for i in paths[0]:
+        x.append(points[i][0])
+        y.append(points[i][1])
+    
+    plt.plot(x, y, 'co')
+
+    a_scale = 0.001
+
+    if num_iters > 1:
+        for i in range(1, num_iters):
+            xi = []; yi = [];
+            for j in paths[i]:
+                xi.append(points[j][0])
+                yi.append(points[j][1])
+
+            plt.arrow(xi[-1], yi[-1], (xi[0] - xi[-1]), (yi[0] - yi[-1]), 
+                    head_width = a_scale, color = 'r', 
+                    length_includes_head = True, ls = 'dashed',
+                    width = 0.001/float(num_iters))
+            for i in range(0, len(x) - 1):
+                plt.arrow(xi[i], yi[i], (xi[i+1] - xi[i]), (yi[i+1] - yi[i]),
+                        head_width = a_scale, color = 'r', length_includes_head = True,
+                        ls = 'dashed', width = 0.001/float(num_iters))
+
+    # Draw the primary path for the TSP problem
+    plt.arrow(x[-1], y[-1], (x[0] - x[-1]), (y[0] - y[-1]), head_width = a_scale, 
+            color ='g', length_includes_head=True)
+    for i in range(0,len(x)-1):
+        plt.arrow(x[i], y[i], (x[i+1] - x[i]), (y[i+1] - y[i]), head_width = a_scale,
+                color = 'g', length_includes_head = True)
+
+    #Set axis too slitghtly larger than the set of x and y
+    if dataset == 'yk11':
+        plt.xlim(-7.6, -7.9)
+        plt.ylim(110.2, 110.8)
+    elif dataset == 'berlin52':
+        plt.xlim(0, 1900)
+        plt.ylim(0, 1200)
+    elif dataset == 'else':
+        plt.xlim(0, max(x)*1.1)
+        plt.ylim(0, max(y)*1.1)
+
+    plt.title('Route map visualization of {0} dataset'.format(dataset))
+    plt.show()
